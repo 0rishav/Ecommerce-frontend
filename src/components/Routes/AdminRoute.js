1e630834bc9051f7a975fceba7e08075915dcbd0
Rishav Raj <96721375+0rishav@users.noreply.github.com>
2024-04-28 21:12:21 +0530

    Update AdminRoute.js

--- src/components/Routes/AdminRoute.js
+++ src/components/Routes/AdminRoute.js
@@ -10,7 +10,7 @@ export default function AdminRoute(){
 
     useEffect(()=>{
         const authCheck = async() => {
-            const res = await axios.get("http://localhost:8000/api/v1/auth/admin-auth")
+            const res = await axios.get("https://ecommerce-server-1fgm.onrender.com/api/v1/auth/admin-auth")
             console.log(res)
             if(res.data.ok){
                 setOk(true)
